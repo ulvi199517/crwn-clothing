14783f63e700ecc3527b0b0d3612df1c02a7bb84
ulvi_199517 <ulvi199517@gmail.com>
2020-10-23 17:52:09 -0400

    updated shop data with a new one, updated cart utils to group items in cart reducer with adding feature of quantity control

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 // import CollectionPreview from '../collection-preview/collection-preview.component';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id,name,price,imageUrl}) => (
-    <div className='collection-item'>
-        <div 
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-            />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            
-    </div>
-)
-export default CollectionItem;
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
+    return(
+        <div className='collection-item'>
+            <div 
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+                />
+                <div className='collection-footer'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{price}</span>
+                </div>
+                <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
+                
+        </div>
+    )
+}
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+export default connect(null, mapDispatchToProps)(CollectionItem);
