0d86a35b7ef4ac278186c9f4e93ba421f1dd6b0c
ulvi_199517 <ulvi199517@gmail.com>
2020-10-23 13:34:44 -0400

    updating app to redirect to home page if user is signed in and changing user action types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -39,15 +39,24 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInandSignUpPage}/>
+          <Route 
+              exact 
+              path='/signin' 
+              render={() => this.props.currentUser ? 
+                (<Redirect to='/'/>) :
+                (<SignInandSignUpPage/>)
+            }/>
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = ({user})=> ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
