169c9e46317bea57ec87f7ade026c6f85b4edb55
ulvi_199517 <ulvi199517@gmail.com>
2020-10-23 13:54:56 -0400

    created cart-icon component and located it in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
 
@@ -19,6 +20,7 @@ const Header = ({currentUser}) => (
                  :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
             
             
         </div>
