390929fb501d301ae279776e6f38586c6dfd1334
ulvi_199517 <ulvi199517@gmail.com>
2020-10-26 22:55:15 -0400

    created checkout-item component which shows items of our cart

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import './checkout.styles.scss';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
-
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
@@ -25,7 +25,7 @@ const CheckoutPage = ({cartItems, total}) => (
         </div>
         {
             cartItems.map(cartItem => 
-                cartItem.name
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
                 )
         }
         <div className='total'>
