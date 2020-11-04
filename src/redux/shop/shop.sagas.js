ad840e9a4777bf3d4c212f5f234c5e3769e45e91
ulvi_199517 <ulvi199517@gmail.com>
2020-11-03 22:37:58 -0500

    added functionality to the cart dropdown to be cleared out when user signs out

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {takeLatest, call, put} from 'redux-saga/effects';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
 import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 import {fetchCollectionsSuccess, fetchCollectionsFailure} from './shop.actions';
@@ -15,4 +15,9 @@ export function* fetchCollectionsAsync() {
 };
 export function* fetchCollectionsStart() {
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+};
+export function* shopSagas() {
+    yield all([
+        call(fetchCollectionsStart)
+    ])
 };
